90a6e0aba5867862a46383eced0899532c197d59
GaRam We <dnlrkfka0712@naver.com>
2018-06-18 07:08:01 +0000

    style errors and messages partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,8 +14,9 @@ class ArticlesController < ApplicationController
     # redirect_to article_path(@article)
     # render plain: params[:article].inspect
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
+      # :success는 초록색 배경을 뜻한다.
     else
       render 'new'
     end
@@ -27,7 +28,7 @@ class ArticlesController < ApplicationController
   def destroy
    
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
   
@@ -38,7 +39,7 @@ class ArticlesController < ApplicationController
   def update
     
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
