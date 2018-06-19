9cf66ea64dac0ea7c72e3ae3e027ab4a7c76984f
GaRam We <dnlrkfka0712@naver.com>
2018-06-19 01:23:39 +0000

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+
   root 'pages#homepage'
 
   get '/about' => 'pages#about'
   
   resources :articles
+  
+  get 'signup' => 'users#new'
+  # post 'users' => 'users#create'
+  resources :users, except: [:new]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
