a013d6cb3b11d3c0228bde282af80787aad0a38d
GaRam We <dnlrkfka0712@naver.com>
2018-06-19 02:42:20 +0000

    Create show action and add gravatar image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80})
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "img-circle")
+    # class: 뒤꺼는 부트스트랩에서 제공하는거 씀.
+  end
 end
