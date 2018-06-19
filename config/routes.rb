e8c06328cb61bbffb6edfd915e394f5abd25c380
GaRam We <dnlrkfka0712@naver.com>
2018-06-19 22:58:37 +0000

    Update current_user to be article creator and make authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
   # post 'users' => 'users#create'
   resources :users, except: [:new]
   
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
