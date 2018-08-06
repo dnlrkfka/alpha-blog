9084fddf434b8ed3638229326a63d2cc47a06578
GaRam We <dnlrkfka0712@naver.com>
2018-08-06 10:03:31 +0000

    fix tests and update navbar with categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,26 +1,32 @@
-# require 'test_helper'
+require 'test_helper'
 
-# class CreateCategoriesTest < ActionDispatch::IntegrationTest
+class CreateCategoriesTest < ActionDispatch::IntegrationTest
   
-#   test "get new category form and create category" do
-#     get new_category_path
-#     assert_template 'categories/new'
-#     assert_difference 'Category.count', 1 do
-#       post categories_path, params:{ category:{name: "sports"}}
-#       follow_redirect!
-#     end
-#     assert_template 'categories/index'
-#     assert_match "sports", response.body
-#   end
+  def setup
+     @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+  end
   
-#   test "invalid category submission results in failure" do
-#     get new_category_path
-#     assert_template 'categories/new'
-#     assert_no_difference 'Category.count' do
-#       post categories_path, params:{category:{name: " "}}
-#     end
-#     assert_template 'categories/index'
-#     assert_select 'h2.panel-title'
-#     assert_select 'div.panel-body'
-#   end
-# end
+  test "get new category form and create category" do
+    sign_in_as(@user, "password")
+    get new_category_path
+    assert_template 'categories/new'
+    assert_difference 'Category.count', 1 do
+      post categories_path, params:{ category:{name: "sports"}}
+      follow_redirect!
+    end
+    assert_template 'categories/index'
+    assert_match "sports", response.body
+  end
+  
+  test "invalid category submission results in failure" do
+    sign_in_as(@user, "password")
+    get new_category_path
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
+      post categories_path, params:{category:{name: " "}}
+    end
+    assert_template 'categories/index'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+end
